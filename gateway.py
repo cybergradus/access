d8fa51fb8fb1530c7939b621f6f2de8a2ce985d3
Anton Pleshkov <63904606+cybergradus@users.noreply.github.com>
2025-04-14 08:11:32 +0000

    random.uniform()

--- gateway.py
+++ gateway.py
@@ -5,6 +5,6 @@ scale_max = 2.5
 
 def run():
     print('well, well, well...')
-    random_scale = random.random(scale_min, scale_max)
+    random_scale = random.uniform(scale_min, scale_max)
     print(f'{random_scale}:random_scale')
     mc.scale(random_scale, random_scale, random_scale, mc.ls(sl=True))
