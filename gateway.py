e967e48bfe7dec84aeca74fb7d3a4b683f7939ec
Anton Pleshkov <63904606+cybergradus@users.noreply.github.com>
2025-04-14 08:09:01 +0000

    deleted import maya.cmds

--- gateway.py
+++ gateway.py
@@ -1,4 +1,3 @@
-import maya.cmds as mc
 import random
 
 scale_min = 0.1
@@ -7,5 +6,5 @@ scale_max = 2.5
 def run():
     print('well, well, well...')
     random_scale = random.random(scale_min, scale_max)
-    print(f'{random_scale}':random_scale)
+    print(f'{random_scale}:random_scale')
     mc.scale(random_scale, random_scale, random_scale, mc.ls(sl=True))
